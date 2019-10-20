fee977b2c798e18b36fb8917d4f10b753b846c3d
AzizStark <theazizstark@gmail.com>
2019-10-20 22:35:44 +0530

    Changes

---
+++ views/login.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+
+class UserSignup extends React.Component {
+  render() {
+    return (
+      <html>
+        <head><title>Login</title>
+        </head>
+        <body>Login</body>
+        <form id="formAddUser" name="adduser" method="post" action="/userlogin">
+          <input id="inputUserName" type="email" placeholder="email" name="usermail" /> < br/>
+          <input id="inputUserEmail" type="password" placeholder="password" name="password" /> < br/>
+          <button id="btnSubmit" type="submit">Login</button>
+       </form>
+       <form id="redirect" name="redirect" method="get" action="/signup">
+       <button>Sign Up</button>
+       </form>
+      </html>
+    );
+  }
+}
+
+module.exports = UserSignup;
---
+++ views/signup.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+
+class UserSignup extends React.Component {
+  render() {
+    return (
+      <html>
+        <head><title>Signup</title>
+        </head>
+        <body>Signup</body>
+        <form id="formAddUser" name="adduser" method="post" action="/adduser">
+          <input id="inputUserName" type="text" placeholder="username" name="username" required/> < br/>
+          <input id="inputUserEmail" type="email" placeholder="email" name="useremail" required/>< br/>
+          <input id="inputUserEmail" type="password" placeholder="password" name="password" required/> < br/>
+          <button id="btnSubmit" type="submit">Sign Up</button>
+       </form>
+       <form id="redirect" name="redirect" method="get" action="/login">
+       <button>Login</button>
+       </form>
+      </html>
+    );
+  }
+}
+
+module.exports = UserSignup;
