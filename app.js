b08d1ebf535038eb695b00731e690baf46cee84d
AzizStark <theazizstark@gmail.com>
2019-10-21 04:53:16 +0530

    Changes

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
     req.session.token = req.user.token;
-    req.session.email = req.user.profile;
+    req.session.email = req.user.profile.getName();
     res.redirect('/');
   }
 );
