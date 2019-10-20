7f0b5d743ad8272cdf198a81a28cbae1d107eef7
AzizStark <rrahman182@gmail.com>
2019-10-20 16:34:16 +0530

    Changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { titles: 'Express Aziz' });
+  res.render('index', { titles: 'Aziz' });
 });
 
 router.get('/helloworld', function (req, res) {
