ca51ef21d0ba759e9db0a02756696230920b92f0
AzizRahman <rrahman182@gmail.com>
2019-10-21 12:07:52 +0530

    Secured Sensitive API secret key

--- auth.js
+++ auth.js
@@ -7,8 +7,8 @@ module.exports = (passport) => {
         done(null, user);
     });
     passport.use(new GoogleStrategy({
-            clientID: "130057753017-483rt34e57f011f0smptpj0f55im9h6q.apps.googleusercontent.com",
-            clientSecret: "BFLqkUxpGbqIiGY0y6fAFdHU",
+            clientID: "1300577530pps.googleusercontent.com",
+            clientSecret: "fAFdHU",
             callbackURL: 	"https://alpha7shopping.herokuapp.com/auth/google/callback"
         },
         (token, refreshToken, email, done) => {
