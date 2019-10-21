9f154acb1ad9ec3a28765bc49b5997539acfc4fa
AzizStark <theazizstark@gmail.com>
2019-10-21 07:02:41 +0530

    Changes

--- views/signup.jsx
+++ views/signup.jsx
@@ -5,17 +5,22 @@ class UserSignup extends React.Component {
     return (
       <html>
         <head><title>Signup</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         </head>
-        <body>Signup</body>
+        <div style={{margin: "auto",marginTop: "14%",width: 300}}>
+        <h5 style={{textAlign:"center",width: 300}}>Alpha7Shopping Signup</h5>
         <form id="formAddUser" name="adduser" method="post" action="/adduser">
-          <input id="inputUserName" type="text" placeholder="username" name="username" required/> < br/>
-          <input id="inputUserEmail" type="email" placeholder="email" name="useremail" required/>< br/>
-          <input id="inputUserEmail" type="password" placeholder="password" name="password" required/> < br/>
-          <button id="btnSubmit" type="submit">Sign Up</button>
+          <input  class="form-control" style={{width: 300}} id="inputUserName" type="text" placeholder="username" name="username" required/> < br/>
+          <input   class="form-control" style={{width: 300}}id="inputUserEmail" type="email" placeholder="email" name="useremail" required/>< br/>
+          <input   class="form-control" style={{width: 300}} id="inputUserEmail" type="password" placeholder="password" name="password" required/> < br/>
+          <button button style={{marginLeft: "35%"}} class="btn btn-primary" id="btnSubmit" type="submit">Sign Up</button>
        </form>
-       <form id="redirect" name="redirect" method="get" action="/login">
-       <button>See Login Options</button>
+       <form id="redirect" name="redirect" method="get" action="/login"> < br/>
+       <button button style={{marginLeft: "23%"}} class="btn btn-primary">See Login Options</button>
        </form>
+       </div>
       </html>
     );
   }
